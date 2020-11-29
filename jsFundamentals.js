b5727a4adb39dc9f6dc6cc249ebda03b3f6b9e6c
wilsonng21 <wilsongch92@gmail.com>
2020-11-29 16:18:37 -0500

    added spread and rest operator examples

--- jsFundamentals.js
+++ jsFundamentals.js
@@ -28,6 +28,30 @@ const [{color: firstColor}, {color: secondColor}] = game.suspects
 
 const nums = [ 1, 2, 3, 4, 5, 6 ]
 
-// for(let i = 0; i < nums.length; i++) {
-//     console.log(nums[nums.length - 1 - i])
-// }
+for(let i = 0; i < nums.length; i++) {
+    console.log(nums[nums.length - 1 - i])
+}
+
+// spread/rest
+// rest parameters collect all remaining elements into an array
+const createTuple = (a, b, c, ...extraArgs) => {
+    return [[a, b], [c], extraArgs]
+}
+
+console.log(createTuple(1, 2, 3, 4, 5, 6, 7,))
+
+const add = (...args) => { // arrow functions have no 'arguments' keyword
+        let result = 0;
+        for (let arg of args) { result += arg }
+        return result
+}
+    
+add(1) // returns 1
+add(1,2) // returns 3
+console.log(add(1, 2, 3, 4, 5)) // returns 15
+
+// spread operator allows us to unpack elements in an array to single/individual arguments
+// unpacking/spreading, copying
+const arr = [1, 2, 3]
+const arr2 = [...arr]
+const myNames = [...arr, "joykare"]
