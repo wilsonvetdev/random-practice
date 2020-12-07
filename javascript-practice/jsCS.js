a09b4bb36b03bc72f8f68430455888722efeca19
wilsonng21 <wilsongch92@gmail.com>
2020-12-06 21:10:13 -0500

    closure exercises

--- javascript-practice/jsCS.js
+++ javascript-practice/jsCS.js
@@ -50,4 +50,58 @@ const uniqSort = function(arr) {
     return newArray
 }
 
-console.log(uniqSort([4,2,2,3,2,2,2])) // => [2,3,4]
+// console.log(uniqSort([4,2,2,3,2,2,2])) // => [2,3,4] 
+
+
+// Memoization: caching the value that a function returns 
+// Caching: saving a value to an object, array that's not a result of a function 
+
+// Task 1: Write a function, times10, that takes an argument, n, and multiples n times 10
+// a simple multiplication fn
+const times10 = n =>  n * 10 
+
+// console.log('~~~~~~~~~~~~~~TASK 1~~~~~~~~~~~~~~')
+// console.log('times10 returns:', times10(9))
+
+// Task 2: Use an object to cache the results of your times10 function. 
+// protip 1: Create a function that checks if the value for n has been calculated before.
+// protip 2: If the value for n has not been calculated, calculate and then save the result in the cache object.
+
+// const cache = {};
+
+// const memoTimes10 = (n) => {
+//     if(!cache[n]) { //n in(operator) cache
+//         cache[n] = times10(n)
+//     }
+//     return cache[n]
+// }
+
+// console.log('~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~');
+// console.log('Task 2 calculated value:', memoTimes10(9));	// calculated
+// console.log('Task 2 cached value:', memoTimes10(9));	// cached
+
+// Task 3: Clean up your global scope by moving your cache inside your function.
+// protip: Use a closure to return a function that you can call later.
+
+const memoizedClosureTimes10 = () => {
+    const cache = {}
+
+    const memoTimes10 = n => {
+        if(!cache[n]) { //n in(operator) cache
+            cache[n] = times10(n)
+        }
+        return cache
+    }
+
+    return memoTimes10
+}
+
+const memoClosureTimes10 = memoizedClosureTimes10();
+console.log('~~~~~~~~~~~~~~TASK 3~~~~~~~~~~~~~~');
+try {
+    console.log('Task 3 calculated value:', memoClosureTimes10(10));	// calculated
+    console.log('Task 3 cached value:', memoClosureTimes10(10));	// cached
+    console.log(memoClosureTimes10(4))
+} catch(e) {
+    console.error('Task 3:', e);
+}
