e4e610f3f1c1089be274a7c3e27beade929ba1ce
wilsonng21 <wilsongch92@gmail.com>
2020-12-07 11:19:48 -0500

    made memo function generic

--- javascript-practice/jsCS.js
+++ javascript-practice/jsCS.js
@@ -83,25 +83,30 @@ const times10 = n =>  n * 10
 // Task 3: Clean up your global scope by moving your cache inside your function.
 // protip: Use a closure to return a function that you can call later.
 
-const memoizedClosureTimes10 = () => {
+// Task 4: Make your memo function generic and accept the times10 function as a callback rather than defining the n * 10 logic inside the if/else or pulling it in from the global scope.
+
+// protip: Take advantage of the fact that parameters are saved in the closure as well, just like the cache from the previous example.
+
+const memoizedClosureTimes10 = (cb) => {
     const cache = {}
 
-    const memoTimes10 = n => {
-        if(!cache[n]) { //n in(operator) cache
-            cache[n] = times10(n)
+    return n => {
+        if(!cache[n]) { //n in(operator) cache also works ex. if(n in cache)
+            cache[n] = cb(n)
         }
-        return cache
+        return cache[n]
     }
 
-    return memoTimes10
 }
 
-const memoClosureTimes10 = memoizedClosureTimes10();
+const memoClosureTimes10 = memoizedClosureTimes10(times10);
 console.log('~~~~~~~~~~~~~~TASK 3~~~~~~~~~~~~~~');
 try {
     console.log('Task 3 calculated value:', memoClosureTimes10(10));	// calculated
     console.log('Task 3 cached value:', memoClosureTimes10(10));	// cached
     console.log(memoClosureTimes10(4))
+    console.log(memoClosureTimes10(6))
+    console.log(memoClosureTimes10(10))
 } catch(e) {
     console.error('Task 3:', e);
 }
