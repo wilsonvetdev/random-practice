ff4ca13b83c032e646257a2b9c59b06cdf8bba7e
wilsonng21 <wilsongch92@gmail.com>
2020-12-15 21:50:12 -0500

    linked list exercises added

---
+++ javascript-practice/datastructures.js
@@ -0,0 +1,44 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    push(value) {
+
+    }
+
+    pop() {
+
+    }
+
+    _find(value, test=this._test) {
+
+    }
+
+    _test(a, b) {
+        return a === b
+    }
+
+    testIndex(search, __, i) {
+        return search === i
+    }
+
+    get(index) {
+
+    }
+
+    delete(index) {
+
+    }
+
+
+}
